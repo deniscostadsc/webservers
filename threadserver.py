0f158996167201d77e1bfd47c6de9be35c46eec3
Denis Costa <deniscostadsc@gmail.com>
2013-05-28 16:36:29 -0300

    Separando função que trata requisição.

--- threadserver.py
+++ threadserver.py
@@ -12,16 +12,16 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(ADDRESS)
 server_socket.listen(5)
 
+
+def handle_connection(client_socket):
+    data = client_socket.recv(BUFFER_SIZE)
+    client_socket.send('%s' % data)
+    client_socket.close()
+
 try:
     while True:
         client_socket, address = server_socket.accept()
-
-        def response(client_socket):
-            data = client_socket.recv(BUFFER_SIZE)
-            client_socket.send('%s' % data)
-            client_socket.close()
-
-        thread.start_new_thread(response, (client_socket,))
+        thread.start_new_thread(handle_connection, (client_socket,))
 
 except:
     server_socket.close()
